076586fee793616c96bf08ca641c88ce3219d52f
hydrix9 <chadlbourke@yahoo.com>
2022-02-05 11:30:38 -1000

    Delete Talents/Clothie directory

--- Talents/Clothie/SpaceClothie_UnusedSpells.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SpaceRogue_UnusedSpells
-{
-
-
-} //end class SpaceRogue_UnusedSpells
--- Talents/Clothie/SpaceClothie_UnusedTalents.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SpaceRogue_UnusedTalents
-{
-
-} //end class SpaceRogue_UnusedTalents
